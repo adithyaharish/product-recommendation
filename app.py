b356ef7fd953a8ddd477342392f46b3f60289f70
Adithya Harish <49112771+adithyaharish@users.noreply.github.com>
2021-03-17 20:51:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,19 @@
 from flask import Flask, render_template,request   
 import pandas as pd
 import pickle
-import itertools
-import os
-from os  import getcwd
-#import bz2
-#import _pickle as cPickle
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'adithya'
 
-
-directory = getcwd()
-
 smd=pickle.load(open('smd.pkl','rb'))
-#pre_df=pickle.load(open('pre_df.pkl','rb'))
-
-
 
 content=[]
-#read csv, and split on "," the line
+
 for index, row in smd.iterrows():
     content.append(row['product_name'])
 
-content2=list(set(content))[:200]
+content2=list(set(content))[:250]
 
 
 
@@ -42,26 +32,6 @@ titles = smd['product_name']
 indices = pd.Series(smd.index, index=smd['product_name'])
 
 
-'''
-from sklearn.feature_extraction.text import TfidfVectorizer
-tfv = TfidfVectorizer(max_features=None,
-                 strip_accents='unicode',
-                 analyzer='word',
-                 min_df=10,
-                 token_pattern=r'\w{1,}',
-                 ngram_range=(1,3),#take the combination of 1-3 different kind of words
-                 stop_words='english')#removes all the unnecessary characters like the,in etc.
-
-pre_df['description'] = pre_df['description'].fillna('')
-
-#fitting the description column.
-tfv_matrix = tfv.fit_transform(pre_df['description'])
-#converting everythinng to sparse matrix.
-
-from sklearn.metrics.pairwise import sigmoid_kernel
-sig = sigmoid_kernel(tfv_matrix,tfv_matrix)
-'''
-
 
 def similar_prods(title):
 
@@ -73,16 +43,18 @@ def similar_prods(title):
     product_indices = [i[0] for i in sim_scores]
     return titles.iloc[product_indices]
 
-'''
-def product_recommendation(title,sig=sig):    
-  
-    indx = indices[title]
-    sig_scores = list(enumerate(sig[indx]))
-    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
-    sig_scores = sig_scores[1:11]
-    product_indices = [i[0] for i in sig_scores]
-    return pre_df['product_name'].iloc[product_indices]
-'''
+
+pre_df=pickle.load(open('rating.pkl','rb'))
+
+rslt_df = pre_df[pre_df['overall_rating'] > 1.0]  
+
+details = pd.DataFrame(rslt_df, columns =['product_name', 
+                                           'overall_rating']) 
+
+rslt_df = details.sort_values(by = ['overall_rating'],ascending=False).head(10)
+
+rslt_df=dict(rslt_df.values)
+
 
 
 @app.route("/")
@@ -96,22 +68,7 @@ def home():
         if prod_name in content:
             prod_detail = similar_prods(prod_name)
             prod_detail=prod_detail.head(10).to_dict()
-            '''
-            prod_detail_2 = product_recommendation(prod_name)
-            prod_detail_2=prod_detail_2.head(10).to_dict()
             
-            res={}
-            temp={}
-            for key,val in prod_detail.items():
-                if(val in prod_detail_2):
-                    res[key]=prod_detail[key]
-                else: 
-                    temp[key]=prod_detail[key]
-            
-            l=10-len(res)
-            if(l!=0):
-                res.update(dict(itertools.islice(temp.items(),l)))
-            '''
             out1 = dict(list(prod_detail.items())[:5])
             out2 = dict(list(prod_detail.items())[5:])
             
@@ -122,6 +79,15 @@ def home():
         
         return render_template('index.html', content=content2)
 
+
+
+@app.route("/rating", methods=['GET','POST'])
+def rating():
+      
+    out1 = dict(list(rslt_df.items())[:5])
+    out2 = dict(list(rslt_df.items())[5:])
+    return render_template('rating.html',out1=out1,out2=out2)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
